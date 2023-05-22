eaddaacc97258713f59d192f19e9734f8e7b1ef4
Adamdim <101868494+AdamDimErt@users.noreply.github.com>
2023-05-22 18:24:57 +0600

    update

---
+++ src/components/Index.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import Calculator from "../components/calculator/Index";
+import Slider from "../components/slider/index";
+import Feedback from "../components/feedback/Index";
+import Tariff from "../components/tariff/Index";
+import Main from "../components/main/Index";
+
+export default { Calculator, Slider, Feedback, Tariff, Main, Work, Expir };
